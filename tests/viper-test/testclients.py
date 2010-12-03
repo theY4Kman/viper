53a52451d35de1780a9fc22f59dbab6f947b608b
theY4Kman <they4kman@gmail.com>
2010-12-03 16:21:17 -0500

    Added a couple sourcemod.clients tests

--- tests/viper-test/testclients.py
+++ tests/viper-test/testclients.py
@@ -1,5 +1,5 @@
 import unittest
-from sourcemod import clients
+from sourcemod import *
 
 class ClientsModuleTestCase(unittest.TestCase):
   def test_clients_functions_exist(self):
@@ -21,11 +21,27 @@ class ClientsModuleTestCase(unittest.TestCase):
     '''sourcemod.clients Client type exists'''
     self.assert_(hasattr(clients, 'Client') and type(clients.Client) is type,
         'clients.Client does not exist or is not a base type')
+  
+  def test_clients_max(self):
+    '''sourcemod.clients.get_client respects client index limits'''
+    def low_limit():
+      return clients.get_client(-1)
+    def high_limit():
+      return clients.get_client(clients.get_max_clients() + 1)
+    
+    self.assertRaises(ViperError, low_limit)
+    self.assertRaises(ViperError, high_limit)
 
 
 class ClientsTypeTestCase(unittest.TestCase):
+  def test_clients_index(self):
+    '''sourcemod.clients.Client index correct for server console'''
+    server = clients.get_client(0)
+    self.assertEqual(server.index, 0, 'Server console client index should be'
+        ' 0, but is %d' % server.index)
+  
   def test_clients_attributes(self):
-    '''clients.Client attributes exist'''
+    '''sourcemod.clients.Client attributes exist'''
     for name in ['abs_angles', 'abs_origin', 'alive', 'armor', 'avg_choke',
         'avg_choke_in', 'avg_choke_out', 'avg_data', 'avg_data_in',
         'avg_data_out', 'avg_latency', 'avg_latency_in', 'avg_latency_out',
@@ -37,7 +53,7 @@ class ClientsTypeTestCase(unittest.TestCase):
       self.assertTrue(hasattr(clients.Client, name))
   
   def test_clients_functions_exist(self):
-    '''sourcemod.clients functions exist'''
+    '''sourcemod.clients.Client functions exist'''
     for name in ['fake_command', 'is_timing_out', 'kick',
                  'notify_post_admin_check', 'print_center', 'print_console',
                  'print_chat', 'print_hint', 'set_fake_client_convar',
