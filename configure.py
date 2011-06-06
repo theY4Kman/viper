a8cecd63444c7fa040a240686516d9d06ccc70a9
Reuben Morais <reuben.morais@gmail.com>
2011-06-06 15:12:01 -0300

    Added AMBuild based build system for Viper

---
+++ configure.py
@@ -0,0 +1,9 @@
+import sys
+import ambuild.runner as runner
+
+run = runner.Runner()
+run.options.add_option('--enable-debug', action='store_const', const='1', dest='debug',
+                       help='Enable debugging symbols')
+run.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt',
+                       help='Enable optimization')
+run.Configure(sys.path[0])
