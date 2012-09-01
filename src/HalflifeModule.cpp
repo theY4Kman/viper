d81951bbbdf985a68d52f9e07acbd25628340bf9
aiacono <aiacono@calistolabs.com>
2012-09-01 16:33:55 -0700

    Halflife module

--- src/HalflifeModule.cpp
+++ src/HalflifeModule.cpp
@@ -5,25 +5,124 @@
 
 namespace py = boost::python;
 
-py::object halflife_get_game_description(py::tuple argumentsTuple,
-	py::dict keywordsDict) {
-	bool original = false;
-					
-	if(keywordsDict.contains("original")) {
-		original = py::extract<bool>(keywordsDict["original"]);
-	}
-	else if(py::len(argumentsTuple) > 1) {
-		original = py::extract<bool>(argumentsTuple[1]);
+void halflife__log_to_game(std::string text, bool appendNewline = true) {
+	if(appendNewline) {
+		text.append("\n");
 	}
 
+	engine->LogPrint(text.c_str());
+}
+
+std::string halflife__get_game_description(bool original = false) {
 	if(original) {
-		return py::str(gamedll->GetGameDescription());
+		return std::string(gamedll->GetGameDescription());
 	}
 
-	return py::str(SERVER_CALL(GetGameDescription)());
+	return std::string(SERVER_CALL(GetGameDescription)());
+}
+
+void halflife__set_random_seed(int seed) {
+	g_Interfaces.UniformRandomStreamInstance->SetSeed(seed);
+}
+
+float halflife__get_random_float(float minimum = 0.0f, float maximum = 1.0f) {
+	return g_Interfaces.UniformRandomStreamInstance->RandomFloat(minimum, maximum);
+}
+
+int halflife__get_random_int(int minimum, int maximum) {
+	return g_Interfaces.UniformRandomStreamInstance->RandomInt(minimum, maximum);
+}
+
+bool halflife__is_map_valid(std::string map) {
+	return engine->IsMapValid(map.c_str());
+}
+
+bool halflife__is_dedicated_server() {
+	return engine->IsDedicatedServer();
+}
+
+float halflife__get_engine_time() {
+	#if SOURCE_ENGINE >= SE_LEFT4DEAD2
+	float time = Plat_FloatTime();
+#else
+	float time = engine->Time();
+#endif
+	
+	return time;
+}
+
+float halflife__get_game_time() {
+	return g_Interfaces.GlobalVarsInstance->curtime;
+}
+
+int halflife__get_game_tick_count() {
+	return g_Interfaces.GlobalVarsInstance->tickcount;
+}
+
+std::string halflife__get_game_folder_name() {
+	return std::string(g_pSM->GetGameFolderName());
+}
+
+std::string halflife__get_current_map() {
+	return std::string(gamehelpers->GetCurrentMap());
+}
+
+int halflife__precache_model(std::string model, bool preload = false) {
+	return engine->PrecacheModel(model.c_str(), preload);
+}
+
+int halflife__precache_sentence_file(std::string file, bool preload = false) {
+	return engine->PrecacheSentenceFile(file.c_str(), preload);
+}
+
+int halflife__precache_decal(std::string decal, bool preload = false) {
+	return engine->PrecacheDecal(decal.c_str(), preload);
+}
+
+int halflife__precache_generic(std::string genericFile, bool preload = false) {
+	return engine->PrecacheGeneric(genericFile.c_str(), preload);
+}
+
+bool halflife__precache_sound(std::string sound, bool preload = false) {
+	return g_Interfaces.EngineSoundInstance->PrecacheSound(sound.c_str(), preload);
+}
+
+bool halflife__is_model_precached(std::string model) {
+	return engine->IsModelPrecached(model.c_str());
+}
+
+bool halflife__is_generic_precached(std::string genericFile) {
+	return engine->IsGenericPrecached(genericFile.c_str());
+}
+
+bool halflife__is_decal_precached(std::string decal) {
+	return engine->IsDecalPrecached(decal.c_str());
+}
+
+bool halflife__is_sound_precached(std::string sound) {
+	return g_Interfaces.EngineSoundInstance->IsSoundPrecached(sound.c_str());
 }
 
 BOOST_PYTHON_MODULE(halflife) {
-	py::def("get_game_description", py::raw_function(halflife_get_game_description,
-		0));
+	py::def("log_to_game", &halflife__log_to_game, (py::arg("text"), py::arg("append_newline") = true));
+	py::def("set_random_seed", &halflife__set_random_seed, (py::arg("seed")));
+	py::def("get_random_float", &halflife__get_random_float, (py::arg("minimum") = 0.0f, py::arg("maximum") = 1.0f));
+	py::def("get_random_int", &halflife__get_random_int, (py::arg("minimum"), py::arg("maximum")));
+	py::def("is_map_valid", &halflife__is_map_valid, (py::arg("map")));
+	py::def("is_dedicated_server", &halflife__is_dedicated_server);
+	py::def("get_engine_time", &halflife__get_engine_time);
+	py::def("get_game_time", &halflife__get_game_time);
+	py::def("get_game_tick_count", &halflife__get_game_tick_count);
+	py::def("get_game_description", &halflife__get_game_description, (py::arg("original") = false));
+	py::def("get_game_folder_name", &halflife__get_game_folder_name);
+	py::def("get_current_map", &halflife__get_current_map);
+	py::def("precache_model", &halflife__precache_model, (py::arg("model"), py::arg("preload") = false));
+	py::def("precache_sentence_file", &halflife__precache_sentence_file, (py::arg("file"), py::arg("preload") = false));
+	py::def("precache_decal", &halflife__precache_decal, (py::arg("decal"), py::arg("preload") = false));
+	py::def("precache_generic", &halflife__precache_generic, (py::arg("generic"), py::arg("preload") = false));
+	py::def("precache_sound", &halflife__precache_sound, (py::arg("sound"), py::arg("preload") = false));
+	py::def("is_model_precached", &halflife__is_model_precached, (py::arg("model")));
+	py::def("is_generic_precached", &halflife__is_generic_precached, (py::arg("generic")));
+	py::def("is_decal_precached", &halflife__is_decal_precached, (py::arg("decal")));
+	py::def("is_sound_precached", &halflife__is_sound_precached, (py::arg("sound")));
 }
